0dbbabaa81ca52a5426b5eded2e985b973130e40
Your Name <dinca.toni@gmail.com>
2024-02-06 14:50:23 +0200

    requests validator refactored

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -2,34 +2,7 @@ import { NextFunction, Request, Response, Router } from "express";
 import { getUserListController, getUser } from "./user.controllers";
 import { IdInParams } from "./idInParams";
 import { AnyZodObject, ZodError } from "zod";
-
-interface RequestValidators {
-  params?: AnyZodObject;
-  body?: AnyZodObject;
-  query?: AnyZodObject;
-}
-
-const validateRequest = (validators: RequestValidators) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      if (validators.params) {
-        req.params = await validators.params.parseAsync(req.params);
-      }
-      if (validators.body) {
-        req.body = await validators.body.parseAsync(req.body);
-      }
-      if (validators.query) {
-        req.query = await validators.query.parseAsync(req.query);
-      }
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        res.status(422);
-      }
-      next(error);
-    }
-  };
-};
+import validateRequest from "../middleware/validateRequest";
 
 const router = Router();
 
